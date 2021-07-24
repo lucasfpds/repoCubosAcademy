99691c75e35fc6d60e51ead5b3fea78485e340a2
LUCAS SANTOS <lucasfpd.santos@gmail.com>
2021-07-24 12:24:17 -0300

    Exercicio Finalizado

--- 2º Módulo/02_front-eventos/classe-03/script.js
+++ 2º Módulo/02_front-eventos/classe-03/script.js
@@ -3,6 +3,7 @@ const modal = document.querySelector('.modal')
 const botaoCancelar = document.querySelector('.cancelar')
 const botaoConfirmar = document.querySelector('.confirmar')
 
+
 btn.addEventListener('click',inscrever);
 
 function inscrever (event){
@@ -10,18 +11,24 @@ function inscrever (event){
     btn.classList.add('inscrito')
 
     btn.removeEventListener('click', inscrever);
-    btn.addEventListener('click',function (event){
-        modal.classList.remove('escondido')
-
-    })
-
-    botaoCancelar.addEventListener('click', function(event){
-        modal.classList.add('escondido')
-    })
-
-    botaoConfirmar.addEventListener('click', function(event){
-        modal.classList.add('escondido')
-        btn.textContent = 'INSCREVER-SE'
-        btn.classList.remove('inscrito')
-    })
+    btn.addEventListener('click', abrirModal);
+    
 }
+
+function abrirModal (event){    
+    modal.classList.remove('escondido')
+}
+
+botaoCancelar.addEventListener('click', function(event){
+    modal.classList.add('escondido')
+})
+
+
+
+botaoConfirmar.addEventListener('click', function(event){
+    modal.classList.add('escondido')
+    btn.textContent = 'INSCREVER-SE'
+    btn.classList.remove('inscrito')
+    btn.removeEventListener('click', abrirModal)
+    btn.addEventListener('click',inscrever);
+})
