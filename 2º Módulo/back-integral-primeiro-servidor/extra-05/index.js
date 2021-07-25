7349b9a748b334de388e36d5aee7082e373ff7b5
Lucas Fernandes Paixão Dos Santos <lucasfpd.santos@gmail.com>
2021-07-25 16:21:46 -0300

    Exercicio finalizado

---
+++ 2º Módulo/back-integral-primeiro-servidor/extra-05/index.js
@@ -0,0 +1,62 @@
+
+const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+
+const express = require('express');
+
+const app = express();
+
+let i = 0
+
+app.get('/', (req, res) =>{
+
+    res.send('É a vez de ' + jogadores[i] + ' jogar!')
+
+    i === jogadores.length-1 ? i = 0 : i += 1
+});
+
+
+app.get('/remover', (req, res) =>{
+const index = Number(req.query.indice)
+
+ if(jogadores[index] == undefined){
+     res.send('Não existe jogador no índice informado ('+index+') para ser removido.')
+} else {
+jogadores.splice(index,1)
+res.send(jogadores)
+}
+
+});
+
+
+app.get('/adicionar', (req, res) =>{
+    const index = Number(req.query.indice);
+    
+    const queryNome = req.query.nome;
+    const nome = queryNome[0].toUpperCase() + queryNome.substr(1)
+
+    //res.send(jogadores)
+
+ if(req.query.indice == undefined){
+     
+    jogadores.push(nome)
+
+    } else if (jogadores[index] == undefined){
+
+        res.send('O índice informado ('+index+') não existe no array. Novo jogador não adicionado.')
+
+    } else {
+
+        for(let i = jogadores.length; i >= index; i--){
+        
+          jogadores[i] = jogadores[i-1]
+        
+          i == index ? jogadores[i] = nome : null;
+        }
+    }
+
+    res.send(jogadores)
+});
+
+
+
+app.listen(8000)
