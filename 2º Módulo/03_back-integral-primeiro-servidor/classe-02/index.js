30ea9d1a780c0b4034ed26d7217318339860eed5
Lucas Fernandes Paixão Dos Santos <lucasfpd.santos@gmail.com>
2021-07-25 16:23:40 -0300

    Exercício finalizado

---
+++ 2º Módulo/03_back-integral-primeiro-servidor/classe-02/index.js
@@ -0,0 +1,39 @@
+const alunos = [
+    {
+        nome: "Paula",
+        apelido: "Paulete",
+        idade: 26,
+        signo: "Áries",
+        bandaFavorita: "Rolling Stones",
+        email: "paulete123@gmail.com",
+        serieFavorita: "Dark",
+        curso: "PDZ",
+        roupaDeDormir: "pijama"
+    },
+    {
+        nome: "Felisberto",
+        apelido: "Betinho",
+        idade: 20,
+        signo: "Sargitário",
+        bandaFavorita: "Madonna",
+        email: "betinho.felis@gmail.com",
+        serieFavorita: "Um maluco no pedaço",
+        curso: "PDZ",
+        roupaDeDormir: "só cueca"
+    }
+];
+
+
+let newArr = []
+for(i of alunos){
+const {nome, idade, email, curso} = i
+
+  newArr.push({
+    nome: nome,
+    idade: idade,
+    email: email,
+    curso: curso
+  })
+}
+
+console.log(newArr)
