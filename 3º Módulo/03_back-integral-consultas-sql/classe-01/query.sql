fb3dc10315fcea073b4fc7055262b491e32c4b5c
Lucas Fernandes <lucasfpd.santos@gmail.com>
2021-09-03 18:47:56 -0300

    Exercicios finalizados

---
+++ 3º Módulo/03_back-integral-consultas-sql/classe-01/query.sql
@@ -0,0 +1,18 @@
+SELECT compositor, id FROM musicas;
+SELECT composicao, tempo id FROM musicas WHERE tempo/60 > 4;
+SELECT composicao, compositor, id FROM musicas WHERE id BETWEEN 47 AND 123;
+SELECT * FROM musicas WHERE compositor is not NULL AND tempo/60 < 5 AND compositor != 'Bach';
+SELECT composicao, tempo FROM musicas WHERE compositor = 'Mozart' OR compositor = 'Bach';
+SELECT * FROM musicas ORDER BY id DESC;
+SELECT * FROM musicas ORDER BY tempo DESC;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 5;
+SELECT * FROM musicas ORDER BY tempo DESC LIMIT 10;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 10 OFFSET 5;
+SELECT * FROM musicas LIMIT 10 OFFSET 30;
+SELECT * FROM musicas LIMIT 12 OFFSET 60;
+SELECT DISTINCT compositor FROM musicas WHERE compositor IS NOT NULL;
+SELECT DISTINCT compositor, composicao FROM musicas;
+SELECT * FROM musicas WHERE compositor LIKE 'Bra%';
+SELECT * FROM musicas WHERE ritmo LIKE '%troppo';
+SELECT * FROM musicas WHERE composicao ILIKE '%quartet%';
+SELECT * FROM musicas WHERE composicao NOT ILIKE '%quintet%';
